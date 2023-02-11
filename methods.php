66c720d8eaaa54f6dda90e51dfe0a2501566ea1a
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-02-11 19:43:19 +0300

    Adding methods file

--- methods.php
+++ methods.php
@@ -1,7 +1,9 @@
 <?php
     // Syntax: class className{}
 
-
+    /**
+     * We call them functions when outside a class and methods when inside a class
+     */
     class xxx {
 
         // Methods inside here
