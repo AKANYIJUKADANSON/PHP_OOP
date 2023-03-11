d0d74e6e482ebe620fba8d8b153b24be27870f0e
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-03-11 18:31:40 +0300

    Added inheritance, access modifiers, static modifiers .....

---
+++ include/nav.php
@@ -0,0 +1,17 @@
+<div class="bg-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <div class="container-fluid">
+
+                        <a class="navbar-brand" href="#">
+                            <h3>OOP_PHP prac</h3>
+                        </a>
+
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- Including the header.php file -->
+<?php include('include/header.php'); ?>
+
+<!-- Everything to display on the page is put here -->
+
+<?php include('include/nav.php') ?>
+
+<ul class="d-block navbar-nav ms-4 mb-2 mb-lg-0">
+
+    <li class="nav-item"><a href="class.php" class="nav-link active">A class</a></li>
+    <li class="nav-item"><a href="methods.php" class="nav-link ">Methods</a></li>
+
+    <li class="nav-item"><a href="class-instantiation.php" class="nav-link active">A Class-instantiation</a></li>
+    <!-- <li class="nav-item"><a href="methods.php" class="nav-link ">Methods</a></li>
+    <li class="nav-item"><a href="class.php" class="nav-link active">A class</a></li>
+    <li class="nav-item"><a href="methods.php" class="nav-link ">Methods</a></li>
+    <li class="nav-item"><a href="class.php" class="nav-link active">A class</a></li>
+    <li class="nav-item"><a href="methods.php" class="nav-link ">Methods</a></li> -->
+
+    
+
+</ul>
+<!-- Including the footer.php file -->
+<?php include('include/footer.php'); ?>
